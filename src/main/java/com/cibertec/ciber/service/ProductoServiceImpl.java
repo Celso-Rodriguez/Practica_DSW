adbbdb727bc2b3e50a457f5b3a779b93f13415e6
Celso-Rodriguez <celso.19rod@gmail.com>
2024-06-17 22:38:49 -0500

    AVANCE DE PRODUCTO

--- src/main/java/com/cibertec/ciber/service/ProductoServiceImpl.java
+++ src/main/java/com/cibertec/ciber/service/ProductoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cibertec.ciber.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,21 @@ public class ProductoServiceImpl implements ProductoService {
     @Override
     public List<Producto> listaTodos() {
         return repository.findAll();
+    }
+
+    @Override
+    public Optional<Producto> obtenerProductoPorId(int id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Producto agregarActualizarProducto(Producto producto) {
+        return repository.save(producto) ;
+    }
+
+    @Override
+    public void eliminarProducto(int id) {
+        repository.deleteById(id);
     }
     
 }
